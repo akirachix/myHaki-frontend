5efbecbfa574dd80a4f23589326d17fa34a16a55
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-09-16 09:59:24 +0300

    Added the shared components of myhaki dashboard

--- myhaki/src/app/page.tsx
+++ myhaki/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Sidebar from "./shared-components/SideBar";
-import Layout from "./shared-components/Layout";
+
 
 export default function Home() {
   return (
     <div >
-      <Sidebar />
       <h1>Welcome to MyHaki dashboard!</h1>
     </div>
   );
