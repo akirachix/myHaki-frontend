fd6be6f3c8d801cecd8b77fdaa6baa2f4394703a
Fiona Wesonga <fionahwesonga@gmail.com>
2025-09-24 11:54:13 +0300

    Fix CI/CD pipeline tests

--- myhaki/src/app/shared-components/Layout/index.tsx
+++ myhaki/src/app/shared-components/Layout/index.tsx
@@ -1,18 +1,3 @@
-// import Sidebar from "../SideBar/index";
-
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <div className="dashboard-layout flex">
-//       <div className="flex-shrink-0 w-[250px] mt-0">
-//         <Sidebar />
-//       </div>
-//       <main className="main-content flex-1 px-8">
-//         {children}
-//       </main>
-//     </div>
-//   );
-// }
-// Layout.tsx
 import Sidebar from "../SideBar/index";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
